2985e4bb2aafb7d4e9c4880cb9ac850eb9e3f623
qhung23125005 <xinquyhung91005@gmail.com>
2025-06-27 19:47:10 +0700

    add single product display

---
+++ components/Homescreen/CoffeeProduct.tsx
@@ -0,0 +1,46 @@
+import { Dimensions, Image, StyleSheet, Text, View } from 'react-native';
+
+interface CoffeeProduct {
+    url: string;
+    name: string;
+}
+
+const { width } = Dimensions.get('window');
+const itemWidth = width * 0.3; // 40% of the screen width
+
+export default function CoffeeProduct({url, name}: CoffeeProduct) {
+
+    return (
+        <View style={styles.container}>
+            <Image source={{uri: url} } style={styles.image} resizeMode='contain' />
+            <Text style={styles.productName}>{name}</Text>
+            
+        </View>
+    );
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'white',
+        borderRadius: 15,
+        marginHorizontal: '5%',
+        padding: 10,
+        marginBottom: '10%',
+        width: '40%'
+    },
+    productName: {
+        color: 'black',
+        marginTop: '2%',
+        fontSize: 12,
+        marginBottom: 10,
+    },
+    image: {
+        width: itemWidth,
+        height: undefined, // Maintain aspect ratio
+        aspectRatio: 1, // Adjust this value based on your image's aspect ratio
+        borderRadius: 10,
+    },
+});
