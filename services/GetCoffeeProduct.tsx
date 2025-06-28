7787e4784c3c0258be3bdea9fe91341b12f5d980
qhung23125005 <xinquyhung91005@gmail.com>
2025-06-28 14:54:52 +0700

    update get product

--- services/GetCoffeeProduct.tsx
+++ services/GetCoffeeProduct.tsx
@@ -4,13 +4,17 @@ export async function getCoffeeProduct() {
   try {
     const { data, error } = await supabase
       .from('Coffee')
-      .select('coffee_name, image_url')
+      .select('coffee_name, image_url, price')
 
     if (error) {
       throw error;
     }
-    console.log('Coffee product fetched successfully:', data);
-    return data; 
+    const mappedData = data.map((item: { coffee_name: string; image_url: string , price: number}) => ({
+      name: item.coffee_name,
+      url: item.image_url,
+      price: item.price, // Assuming you want to include price as well
+    }));
+    return mappedData; 
   } catch (error) {
     console.error('Error fetching coffee product:', error);
     throw error; // Re-throw the error for further handling
